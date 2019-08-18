a58de63ffe227ea1200ae0e597a77c7ea5a7bcbc
tkidder <no.name@google.com>
2019-08-17 22:44:05 -0500

    more proper setup of unit tests, added multiple vote check test, resetting vote count before each test

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,20 +1,30 @@
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
+  const component = new VoteComponent();
+
+  beforeEach(() => {
+    component.totalVotes = 0;
+  });
+
   it('should increase total votes when upvoted', () => {
-    const component = new VoteComponent();
     component.upVote();
     expect(component.totalVotes).toBe(1);
   });
 
+  it('should increase 10 times when upvoted 10 times', () => {
+    for (let i = 0; i < 10; i++) {
+      component.upVote();
+    }
+    expect(component.totalVotes).toBe(10);
+  });
+
   it('should not decrease total votes when value is 0 and downvoted', () => {
-    const component = new VoteComponent();
     component.downVote();
     expect(component.totalVotes).toBe(0);
   });
 
   it('should decrease total votes when value is greater than 0 and downvoted', () => {
-    const component = new VoteComponent();
     component.upVote();
     component.downVote();
     expect(component.totalVotes).toBe(0);
